7c43e28686013cbba19e2a98f84cd5ea211faa92
Dariy <d.kordiyak@gmail.com>
2023-02-25 14:04:18 +0200

    Add referral info

--- payments.py
+++ payments.py
@@ -12,17 +12,23 @@ dp = Dispatcher(bot)
 
 PRICE = types.LabeledPrice(label="Купити", amount=200*100)
 
+# commands
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     await bot.send_message(message.chat.id,
                            "Привіт, я допомагаю закривати сесію"
                            " Я можу продати тобі відповіді на твої запитання"
-                           " Напиши /buy щоб купити відповіді, /terms для умов")
+                           " Напиши /buy щоб купити відповіді, /terms для умов, /referral для генерації рефералки")
 
 @dp.message_handler(commands=["terms"])
 async def process_terms_command(message: types.Message):
     await bot.send_message(message.chat.id,
                            "Купіть відповіді на ваші запитання, оплата дає вам 3 відповіді на запитання")
+    
+@dp.message_handler(commands=["referral"])
+async def process_terms_command(message: types.Message):
+    await bot.send_message(message.chat.id,
+                           "Згенерувати реферальне посилання")
 
 # buy
 @dp.message_handler(commands=["buy"])
