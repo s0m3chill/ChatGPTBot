136848f11e6f4479fd6d41ab91b44d8dd19b448b
markiyanch <markiyanch@gmail.com>
2023-02-26 12:20:22 +0100

    Full referral link functionality using dictionary+list

---
+++ referrals.py
@@ -0,0 +1,25 @@
+import config
+from main import *
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+
+# setup
+from aiogram import types
+from aiogram.types.message import ContentType
+
+# this is for testing purposes only, we'll need to create a db table for this shit
+referral_count = {}  # dictionary to keep track of the referral count
+referred_users = []  # list to keep track of referred users
+
+
+def encode_payload(payload: str) -> str:
+    """Encode payload with URL-safe base64url."""
+    payload = str(payload)
+    bytes_payload: bytes = urlsafe_b64encode(payload.encode())
+    str_payload = bytes_payload.decode()
+    return str_payload.replace("=", "")
+
+def decode_payload(payload: str) -> str:
+    """Decode payload with URL-safe base64url."""
+    payload += "=" * (4 - len(payload) % 4)
+    result: bytes = urlsafe_b64decode(payload)
+    return result.decode()
