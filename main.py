0804affeee2e75d2e95b32b914fae6d5b02ff33c
Lubomyr <bobroliu@fit.cvut.cz>
2023-03-13 22:28:39 +0100

    Changed all commands to buttons,some text changes and keyboards changes

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import logging
 import database
 import keyboards as kb
 import openai
-
 # setup
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.message import ContentType
-from aiogram.contrib.fsm_storage.mongo import MongoStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -16,25 +15,18 @@ from aiogram.types import ParseMode
 from aiogram.utils import executor
 from aiogram.utils.markdown import text
 from aiogram.dispatcher import Dispatcher
-
 logging.basicConfig(level=logging.INFO)
-
 openai.api_key = config.OPENAI_TOKEN
-
 # init
 bot = Bot(token=config.TELEGRAM_TOKEN)
-
+storage = MemoryStorage()
+# Create dispatcher object
+dp = Dispatcher(bot, storage=storage)
 #initialize mongoDB
 DataStorage = database.DataStore()
-
-# Create dispatcher object
-dp = Dispatcher(bot, storage=MongoStorage(uri=config.MONGODB_CONNECTION_STRING, db_name='CheatQuestionBot'))
-
 # Define states
 class ChatState(StatesGroup):
     waiting_for_message = State()
-    processing_question = State()
-
 # commands
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
@@ -54,7 +46,6 @@ async def cmd_start(message: types.Message):
             # Increment the referral counter for the user who referred someone
             referrals_counter = DataStorage.getReferrals(referral_user_id) + 1
             DataStorage.updateReferrals(referral_user_id, referrals_counter)
-
             # Check if the referral count has reached config.QUESTIONS_COUNT and send congratulations message
             if referrals_counter == config.REFERRALS_NEEDED:
                 DataStorage.updateReferrals(referral_user_id,0)
@@ -65,24 +56,16 @@ async def cmd_start(message: types.Message):
                            "Привіт, я допомагаю закривати сесію та знаю відповіді на всі твої запитання)\n"
                            f"Кожні 100 гривень дозволяють отримати {config.QUESTIONS_COUNT} відповіді\n"
                            f"Також для отримання 1 безкоштовоної відповіді, створи реферальне посилання та розішли його {config.REFERRALS_NEEDED} друзям. Після їхньої реєстрації ти отримаєш безкоштовну відповідь\n"
-                           "Напиши /get <запитання> щоб поствити запитання\n/buy щоб купити відповіді\n/terms для пере\n/ref_link для генерації рефералки\n/referrals для перевірки кількості зареференних юзерів\n/questions для перевірки кількості питань\n/cancel відмінити генерацію відповіді"
                            , reply_markup=kb.greet_kb)
 
 @dp.message_handler(Text('Інформація ℹ️'))
-async def process_terms_command_button(message: types.Message):
-    await process_terms_command(message)
-
-@dp.message_handler(commands=["terms"])
 async def process_terms_command(message: types.Message):
     await bot.send_message(message.chat.id,
                            f"Кожні 100 гривень дозволяють отримати {config.QUESTIONS_COUNT} відповіді\n"
                            f"Також для отримання 1 безкоштовоної відповіді, створи реферальне посилання та розішли його {config.REFERRALS_NEEDED} друзям. Після їхньої реєстрації ти отримаєш безкоштовну відповідь\n")
 
-@dp.message_handler(Text('Кількість відповідей 🤓'))
-async def check_questions_command_button(message: types.Message):
-    await check_questions_command(message)
 
-@dp.message_handler(commands=["questions"])
+@dp.message_handler(Text('Кількість відповідей 🤓'))
 async def check_questions_command(message: types.Message):
     # Get the referral count for the user
     count = DataStorage.getQuestions(message.from_user.id)
@@ -93,10 +76,6 @@ async def check_questions_command(message: types.Message):
     )
 
 @dp.message_handler(Text('Рефералка 🔗'))
-async def unique_link_command_button(message: types.Message):
-    await unique_link_command(message)
-
-@dp.message_handler(commands=['ref_link'])
 async def unique_link_command(message: types.Message):
     user_id = referrals.encode_payload(message.from_user.id)
     referral_link = f'<a href="tg://resolve?domain=asjhdkaksjbot&start={user_id}">Клінки сюди, щоб стати рефералом</a>'
@@ -109,30 +88,18 @@ async def unique_link_command(message: types.Message):
     )
 
 @dp.message_handler(Text('Запрошені друзі 👯‍♀️'))
-async def check_referrals_command_button(message: types.Message):
-    await check_referrals_command(message)
-
-@dp.message_handler(commands=['referrals'])
 async def check_referrals_command(message: types.Message):
     # Get the referral count for the user
     count = DataStorage.getReferrals(message.from_user.id)
-    if count == 1:
-        # Send the referral count to the user
-        await bot.send_message(
-            message.chat.id,
-            f"{count} людина використала твоє посилання"
-        )
-    else:
-        await bot.send_message(
-            message.chat.id,
-            f"{count} людей використали твоє посилання"
-        )
+    # Send the referral count to the user
+    await bot.send_message(
+        message.chat.id,
+        f"{count} людей використали твоє посилання"
+    )
+
+PRICE = types.LabeledPrice(label="Купити", amount=200*100)
 
 @dp.message_handler(Text('Купити 💸'))
-async def buy_button(message: types.Message):
-    await buy(message)
-
-@dp.message_handler(commands=["buy"])
 async def buy(message: types.Message):
     if config.PAYMENT_TOKEN.split(":")[1] == 'TEST':
         await bot.send_message(message.chat.id, "Test payment!")
@@ -154,14 +121,12 @@ async def buy(message: types.Message):
         start_parameter="one-month-subscription",
         payload="test-invoice-payload"
     )
-
 # pre checkout (10 seconds to answer)
 @dp.pre_checkout_query_handler(lambda query: True)
 async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
     await bot.answer_pre_checkout_query(pre_checkout_q.id, 
                                         ok=True,
                                         error_message="Упс, щось пішло не так")
-
 # payment completion
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: types.Message):
@@ -172,8 +137,8 @@ async def successful_payment(message: types.Message):
     questions_counter = DataStorage.getQuestions(message.from_user.id) + config.QUESTIONS_COUNT
     DataStorage.updateQuestions(message.from_user.id, questions_counter)
     await bot.send_message(message.chat.id, f"Оплата по сумі {message.successful_payment.total_amount // 100} {message.successful_payment.currency} пройшла")
-    await ChatState.waiting_for_message.set()
-
+class ChatState(StatesGroup):
+    waiting_for_message = State()
 # Define handler for messages
 @dp.message_handler(Text(equals='cancel', ignore_case=True), state=ChatState.waiting_for_message)
 async def cancel_handler(message: types.Message, state: FSMContext):
@@ -182,56 +147,31 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply("Операцію скасовано.")
 
 @dp.message_handler(Text('Поставити запитання ❓'))
-async def start_handler_button(message: types.Message):
-    await start_handler(message)
-
-@dp.message_handler(commands=['get'])
 async def start_handler(message: types.Message):
     if DataStorage.checkQuestionsLeft(message.from_user.id):
         # Ask the user to send a message to start the conversation
-        await message.reply("Задавай своє питання")
+        await message.reply("Напиши /get <запитання> щоб задати питання\n/buy щоб купити відповіді\n/terms для умов\n/ref_link для генерації рефералки\n/referrals для перевірки кількості зареференних юзерів\n/questions для перевірки кількості питань\n/cancel відмінити генерацію відповіді")
         # Set the state to waiting_for_message
         # This code should be done after successful payment
         await ChatState.waiting_for_message.set()
     else:
         await message.reply("Та заплати вже, йой :(")
-
 @dp.message_handler(state=ChatState.waiting_for_message)
-async def handle_question(message: types.Message, state: FSMContext):
-    await state.update_data(question=message.text)
-    await ChatState.processing_question.set()
-    await bot.send_message(
-        message.chat.id,
-        f"Чи ти підтверджуєш це питання?\n<b>{message.text}</b>",
-        parse_mode='HTML',
-        reply_markup=kb.question_kb
-    )
-
-@dp.callback_query_handler(text="confirm_no", state=ChatState.processing_question)
-async def ask_question_no(call: types.CallbackQuery, state: FSMContext):
-    await call.answer("Confirming no")
-    await state.finish()
-    await call.message.answer("Викличте /get ще раз")
-
-@dp.callback_query_handler(text="confirm_yes", state=ChatState.processing_question)
-async def ask_question_yes(call: types.CallbackQuery, state: FSMContext):
-    await call.answer("Confirming yes")
-    user_data = await state.get_data()
+async def handle_message(message: types.Message, state: FSMContext):
     # Call the OpenAI API to get the response
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Ти є живою людиною-студентом, який відповідає на запитання сесії"},
-            {"role": "user", "content": user_data['question']}
+            {"role": "user", "content": message.text}
         ]
     )
     # Send the response back to the user
-    await call.message.answer(response.choices[0].message.content)
-    count = DataStorage.getQuestions(call.from_user.id) - 1
-    DataStorage.updateQuestions(call.from_user.id, count)
-    # Finish this state
+    await message.answer(response.choices[0].message.content)
+    count = DataStorage.getQuestions(message.from_user.id) - 1
+    DataStorage.updateQuestions(message.from_user.id, count)
+    # Set the state to waiting_for_message
     await state.finish()
-
 # run
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
