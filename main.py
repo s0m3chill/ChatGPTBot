37a1471ec8ed1e2aa6f131fe323a98a9a3bd864a
Dariy <d.kordiyak@gmail.com>
2023-03-16 17:43:31 +0200

    Setup Heroku

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import asyncio
 import logging
 
 from core import bot, dp
+from config import WEBHOOK_URL
+from config import WEBHOOK_PATH
+from config import WEBAPP_HOST
+from config import WEBAPP_PORT
+from aiogram.utils.executor import start_webhook
 from app.handlers.common import register_handlers_common
 from app.handlers.questions import register_handlers_questions
 from app.handlers.payments import register_handlers_payments
 
 logger = logging.getLogger(__name__)
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 async def main():
     logging.basicConfig(
@@ -21,7 +32,15 @@ async def main():
 
     # start
     try:
-        await dp.start_polling()
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+    )
     finally:
         await dp.storage.close()
         await dp.storage.wait_closed()
