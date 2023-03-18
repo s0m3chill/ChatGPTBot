d66370171078580492b6ff22dad7277acd77be50
markiyanch <markiyanch@gmail.com>
2023-03-18 23:52:29 +0100

    changing telegram token for testing

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from aiogram import types
 import os
 
 # main
-TELEGRAM_TOKEN = '5908266258:AAGjwTsYJr_mZS3UILDx8hzpiyhlHCIAquU'
+TELEGRAM_TOKEN = '6149748819:AAEjslsLQo2UMxnaL4H2sXfXHfn_QSDe_5o'
 OPENAI_TOKEN = '5908266258:AAGjwTsYJr_mZS3UILDx8hzpiyhlHCIAquU'
 # deployment
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
