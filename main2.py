45761ad7c9cf8a4f6504298c983037af7bfb639c
markiyanch <markiyanch@gmail.com>
2023-03-23 00:41:47 +0100

    Last Try

--- main2.py
+++ main2.py
@@ -18,8 +18,8 @@ bot = Bot(token=TELEGRAM_TOKEN, loop=loop)
 dp = Dispatcher(bot)
 #просто повтор сказанного пользователем
 @dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+async def echo(commands=['start']):
+    await bot.send_message(message.chat.id, "gege")
 async def on_startup(dp):
     await bot.delete_webhook(dp) 
     await bot.set_webhook(WEBHOOK_URL)
